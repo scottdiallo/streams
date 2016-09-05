24019efd5ebde2640863cebe919b32e57f379551
scott_diallo <diallot@me.com>
2016-09-05 05:59:32 +0000

    authentication complete

--- alphabet.js
+++ alphabet.js
@@ -1,5 +1,17 @@
 var stream = require('stream');
+var Alphabet = require('./alphabet');
+var Cache = require('./cache');
+var alpha = new Alphabet();
+var cache = new Cache('alpha1');
 
+alpha.pipe(cache);
+
+cache.on('finish', function() {
+    console.log('Cache store:');
+    for (var key in Cache.store) {
+        console.log(key, ':', Cache.store[key]);
+    }
+});
 function Alphabet(options) {
     stream.Readable.call(this, options);
     this._start = 'a';
